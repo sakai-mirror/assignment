e5ddc24fbb0dc3974308b33fb2adc5a77414e97d
ajpoland@iupui.edu <ajpoland@iupui.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-04-18 21:19:25 +0000

    svn merge -c 28850 https://source.sakaiproject.org/svn/assignment/trunk U    assignment-tool/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java U    assignment-tool/tool/src/webapp/vm/assignment/chef_assignments_instructor_student_list_submissions.vm svn log -r 28850 https://source.sakaiproject.org/svn/assignment/trunk ------------------------------------------------------------------------ r28850 | zqian@umich.edu | 2007-04-12 10:53:42 -0400 (Thu, 12 Apr 2007) | 1 line
    Fix to SAK-9367:The view items 'assignment by student' does not function properly
    ------------------------------------------------------------------------
    
    
    git-svn-id: https://source.sakaiproject.org/svn/assignment/branches/sakai_2-4-x@29092 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- assignment-tool/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
+++ assignment-tool/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
@@ -2046,8 +2046,6 @@ public class AssignmentAction extends PagedResourceActionII
 
 		add2ndToolbarFields(data, context);
 
-		pagingInfoToContext(state, context);
-
 		String template = (String) getContext(data).get("template");
 		return template + TEMPLATE_INSTRUCTOR_VIEW_STUDENTS_ASSIGNMENT;
 
