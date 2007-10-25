3e10bebaf8bfaa87218ad7045ac2c75c1455a885
rjlowe@iupui.edu <rjlowe@iupui.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-10-25 19:28:08 +0000

    svn merge -r 37384:37385 https://source.sakaiproject.org/svn/assignment/trunk U    assignment-tool/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java in-143-146:~/java/test/oncourse_2-4-x admin$ svn log -r 37385 https://source.sakaiproject.org/svn/assignment/trunk ------------------------------------------------------------------------ r37385 | zqian@umich.edu | 2007-10-25 12:05:12 -0400 (Thu, 25 Oct 2007) | 1 line
    fix to SAK-12047:Upload All/ When only 1 students information is uploaded, the data for other students is being wiped out
    ------------------------------------------------------------------------
    
    
    git-svn-id: https://source.sakaiproject.org/svn/assignment/branches/oncourse_2-4-x@37390 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- assignment-tool/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
+++ assignment-tool/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
@@ -9094,21 +9094,12 @@ public class AssignmentAction extends PagedResourceActionII
 			// constructor the hashtable for all submission objects
 			Hashtable submissionTable = new Hashtable();
 			Assignment assignment = null;
+			List submissions = null;
 			try
 			{
 				assignment = AssignmentService.getAssignment(aReference);
 				associateGradebookAssignment = StringUtil.trimToNull(assignment.getProperties().getProperty(AssignmentService.PROP_ASSIGNMENT_ASSOCIATE_GRADEBOOK_ASSIGNMENT));
-
-				Iterator sIterator = AssignmentService.getSubmissions(assignment).iterator();
-				while (sIterator.hasNext())
-				{
-					AssignmentSubmission s = (AssignmentSubmission) sIterator.next();
-					User[] users = s.getSubmitters();
-					if (users.length > 0 && users[0] != null)
-					{
-						submissionTable.put(users[0].getSortName(), new UploadGradeWrapper("", "", "", new Vector(), new Vector(), "", ""));
-					}
-				}
+				submissions =  AssignmentService.getSubmissions(assignment);
 			}
 			catch (Exception e)
 			{
@@ -9162,7 +9153,7 @@ public class AssignmentAction extends PagedResourceActionII
 						while ((entry=zin.getNextEntry()) != null)
 						{
 							String entryName = entry.getName();
-							if (!entry.isDirectory() && entryName.indexOf("/._") == -1)
+							if (!entry.isDirectory() && entryName.indexOf("/.") == -1)
 							{
 								if (entryName.endsWith("grades.csv"))
 								{
@@ -9233,6 +9224,11 @@ public class AssignmentAction extends PagedResourceActionII
 										if (userName.indexOf("(") != -1)
 										{
 											userName = userName.substring(0, userName.indexOf("("));
+										}
+										userName=StringUtil.trimToNull(userName);
+										if (!submissionTable.containsKey(userName))
+										{
+											submissionTable.put(userName, new UploadGradeWrapper("", "", "", new Vector(), new Vector(), "", ""));
 										}
 									}
 									if (hasComment && entryName.indexOf("comments") != -1)
@@ -9307,9 +9303,9 @@ public class AssignmentAction extends PagedResourceActionII
 			if (state.getAttribute(STATE_MESSAGE) == null)
 			{
 				// update related submissions
-				if (assignment != null)
+				if (assignment != null && submissions != null)
 				{
-					Iterator sIterator = AssignmentService.getSubmissions(assignment).iterator();
+					Iterator sIterator = submissions.iterator();
 					while (sIterator.hasNext())
 					{
 						AssignmentSubmission s = (AssignmentSubmission) sIterator.next();
