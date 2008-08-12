4967e7153bbb065938adb6aaf5090a67b1d63531
zqian@umich.edu <zqian@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-08-12 16:56:34 +0000

    merged in recent trunk changes
    git-svn-id: https://source.sakaiproject.org/svn/assignment/branches/SAK-13843@50606 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- assignment-impl/impl/src/test/org/sakaiproject/assignment/impl/AssignmentServiceTest.java
+++ assignment-impl/impl/src/test/org/sakaiproject/assignment/impl/AssignmentServiceTest.java
@@ -3,18 +3,18 @@
  * $Id$
  ***********************************************************************************
  *
- * Copyright (c) 2003, 2004, 2005, 2006 The Sakai Foundation.
- * 
- * Licensed under the Educational Community License, Version 1.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at
- * 
- *      http://www.opensource.org/licenses/ecl1.php
- * 
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
+ * Copyright 2003, 2004, 2005, 2006, 2007, 2008 Sakai Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ *
+ *       http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
  * limitations under the License.
  *
  **********************************************************************************/
@@ -62,6 +62,9 @@ public class AssignmentServiceTest extends TestCase
 	
 	// attachment reference
 	String attachmentReference = null;
+    
+	// test zip bunction of the JVM
+        boolean testZipFunction = false;
 	
 	String bigAttachmentContentString = null;
 	
@@ -253,7 +256,8 @@ public class AssignmentServiceTest extends TestCase
 	 *
 	 */
 	public void testZipSubmissionsWithoutFlushing() 
-	{	
+	{
+		if( testZipFunction ) {	
 		System.out.println("student number = " + userNumber);
 		System.out.println("attachment size = " + attachmentSize + "KB");
 		System.gc();
@@ -277,6 +281,7 @@ public class AssignmentServiceTest extends TestCase
 			System.out.println("*************");
 			// gc
 			System.gc();
+		}
 		}
 	}
 
