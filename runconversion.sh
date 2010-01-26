da2c94d916f5ced430fb9542d0798d9294e0348b
arwhyte@umich.edu <arwhyte@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2010-01-26 18:32:34 +0000

    SAK-17809 update path and jar names (M2 -> 2.5.6).
    git-svn-id: https://source.sakaiproject.org/svn/assignment/branches/sakai-2.5.6@72609 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- runconversion.sh
+++ runconversion.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 #
 # you could use the runconversion in db/db-util/conversion like this
-# runconversion.sh -j $HOME/.m2/repository/org/sakaiproject/sakai-assignment-api/M2/sakai-assignment-api-M2.jar \
-#        -j $HOME/.m2/repository/org/sakaiproject/sakai-assignment-api/M2/sakai-assignment-api-M2.jar  \
+# runconversion.sh -j $HOME/.m2/repository/org/sakaiproject/sakai-assignment-api/2.5.6/sakai-assignment-api-2.5.6.jar \
+#        -j $HOME/.m2/repository/org/sakaiproject/sakai-assignment-api/2.5.6/sakai-assignment-api-2.5.6.jar  \
 #          $@
 #
 CLASSPATH="$CLASSPATH:$HOME/.m2/repository/commons-logging/commons-logging/1.0.4/commons-logging-1.0.4.jar"
 CLASSPATH="$CLASSPATH:$HOME/.m2/repository/commons-dbcp/commons-dbcp/1.2.2/commons-dbcp-1.2.2.jar"
 CLASSPATH="$CLASSPATH:$HOME/.m2/repository/commons-pool/commons-pool/1.3/commons-pool-1.3.jar"
 CLASSPATH="$CLASSPATH:$HOME/.m2/repository/mysql/mysql-connector-java/3.1.11/mysql-connector-java-3.1.11.jar"
-CLASSPATH="$CLASSPATH:$HOME/.m2/repository/org/sakaiproject/sakai-util-api/M2/sakai-util-api-M2.jar"
-CLASSPATH="$CLASSPATH:$HOME/.m2/repository/org/sakaiproject/sakai-util/M2/sakai-util-M2.jar"
-CLASSPATH="$CLASSPATH:$HOME/.m2/repository/org/sakaiproject/sakai-entity-api/M2/sakai-entity-api-M2.jar"
-CLASSPATH="$CLASSPATH:$HOME/.m2/repository/org/sakaiproject/sakai-entity-util/M2/sakai-entity-util-M2.jar"
-CLASSPATH="$CLASSPATH:$HOME/.m2/repository/org/sakaiproject/sakai-assignment-api/M2/sakai-assignment-api-M2.jar"
-CLASSPATH="$CLASSPATH:$HOME/.m2/repository/org/sakaiproject/sakai-assignment-impl/M2/sakai-assignment-impl-M2.jar"
-CLASSPATH="$CLASSPATH:$HOME/.m2/repository/org/sakaiproject/sakai-db-storage/M2/sakai-db-storage-M2.jar"
-CLASSPATH="$CLASSPATH:$HOME/.m2/repository/org/sakaiproject/sakai-db-conversion/M2/sakai-db-conversion-M2.jar"
-CLASSPATH="$CLASSPATH:$HOME/.m2/repository/org/sakaiproject/sakai-util-log/M2/sakai-util-log-M2.jar"
+CLASSPATH="$CLASSPATH:$HOME/.m2/repository/org/sakaiproject/sakai-util-api/2.5.6/sakai-util-api-2.5.6.jar"
+CLASSPATH="$CLASSPATH:$HOME/.m2/repository/org/sakaiproject/sakai-util/2.5.6/sakai-util-2.5.6.jar"
+CLASSPATH="$CLASSPATH:$HOME/.m2/repository/org/sakaiproject/sakai-entity-api/2.5.6/sakai-entity-api-2.5.6.jar"
+CLASSPATH="$CLASSPATH:$HOME/.m2/repository/org/sakaiproject/sakai-entity-util/2.5.6/sakai-entity-util-2.5.6.jar"
+CLASSPATH="$CLASSPATH:$HOME/.m2/repository/org/sakaiproject/sakai-assignment-api/2.5.6/sakai-assignment-api-2.5.6.jar"
+CLASSPATH="$CLASSPATH:$HOME/.m2/repository/org/sakaiproject/sakai-assignment-impl/2.5.6/sakai-assignment-impl-2.5.6.jar"
+CLASSPATH="$CLASSPATH:$HOME/.m2/repository/org/sakaiproject/sakai-db-storage/2.5.6/sakai-db-storage-2.5.6.jar"
+CLASSPATH="$CLASSPATH:$HOME/.m2/repository/org/sakaiproject/sakai-db-conversion/2.5.6/sakai-db-conversion-2.5.6.jar"
+CLASSPATH="$CLASSPATH:$HOME/.m2/repository/org/sakaiproject/sakai-util-log/2.5.6/sakai-util-log-2.5.6.jar"
 CLASSPATH="$CLASSPATH:$HOME/.m2/repository/log4j/log4j/1.2.9/log4j-1.2.9.jar"
 
 ##### JDBC DRIVER #####
