02ac29dbe65b85b042d95e00d8f30e3c59cfd119
arwhyte@umich.edu <arwhyte@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2010-03-05 15:15:42 +0000

    SAK-18062 merge trunk r73961
    git-svn-id: https://source.sakaiproject.org/svn/assignment/branches/sakai_2-6-x@74271 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- assignment-tool/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
+++ assignment-tool/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
@@ -1921,7 +1921,6 @@ public class AssignmentAction extends PagedResourceActionII
 			if (a.getContent() != null)
 			{
 				gradeType = a.getContent().getTypeOfGrade();
-				context.put("value_SubmissionType", gradeType);
 			}
 		}
 		catch (IdUnusedException e)
