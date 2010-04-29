913df6aade79718cc871c0472ce7ef35bdfdbe08
zqian@umich.edu <zqian@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2010-04-29 14:58:09 +0000

    merge r76839 and r76840 of SAK-18452 into 2.5.x
    git-svn-id: https://source.sakaiproject.org/svn/assignment/branches/sakai_2-5-x@76847 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- assignment-tool/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
+++ assignment-tool/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
@@ -9560,17 +9560,17 @@ public class AssignmentAction extends PagedResourceActionII
 				else if(fileData.length > 0)
 				{	
 					FileOutputStream tmpFileOut = null;
-					
+					File tempFile = null;
 					try
 					{
-						File f = File.createTempFile(String.valueOf(System.currentTimeMillis()),"");
+						tempFile = File.createTempFile(String.valueOf(System.currentTimeMillis()),"");
 						
-						tmpFileOut = new FileOutputStream(f);
+						tmpFileOut = new FileOutputStream(tempFile);
 						tmpFileOut.write(fileData);
 						tmpFileOut.flush();
 						tmpFileOut.close();
 						
-						ZipFile zipFile = new ZipFile(f, "UTF-8");
+						ZipFile zipFile = new ZipFile(tempFile, "UTF-8");
 						Enumeration<ZipEntry> zipEntries = zipFile.getEntries();
 						ZipEntry entry;
 						while (zipEntries.hasMoreElements())						 						
@@ -9740,6 +9740,12 @@ public class AssignmentAction extends PagedResourceActionII
 							} catch (IOException e) {
 								M_log.warn(this + ": Error closing temp file output stream: " + e.toString());
 							}
+						}
+						//clean up the zip file
+						if (tempFile != null && tempFile.exists()) {
+							if (!tempFile.delete()) {
+								M_log.warn("Failed to clean up temp file");
+					}
 						}
 					}
 
