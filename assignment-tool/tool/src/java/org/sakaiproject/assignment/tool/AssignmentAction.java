46f1f998af78da8000b3402e9d8f242be87eaf67
zqian@umich.edu <zqian@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2010-06-07 15:33:52 +0000

    merge r77957 of SAK-18558 into 2.7.0 branch
    git-svn-id: https://source.sakaiproject.org/svn/assignment/branches/sakai-2.7.0@78371 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- assignment-tool/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
+++ assignment-tool/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
@@ -2881,11 +2881,12 @@ public class AssignmentAction extends PagedResourceActionII
 					contextString, assignmentRef));
 			context.put("accessPointUrl", accessPointUrl);
 			
+			int submissionType = a.getContent().getTypeOfSubmission();
 			// if the assignment is of text-only or allow both text and attachment, include option for uploading student submit text
-			context.put("includeSubmissionText", Boolean.valueOf(Assignment.TEXT_ONLY_ASSIGNMENT_SUBMISSION == a.getContent().getTypeOfSubmission() || Assignment.TEXT_AND_ATTACHMENT_ASSIGNMENT_SUBMISSION == a.getContent().getTypeOfSubmission()));
+			context.put("includeSubmissionText", Boolean.valueOf(Assignment.TEXT_ONLY_ASSIGNMENT_SUBMISSION == submissionType || Assignment.TEXT_AND_ATTACHMENT_ASSIGNMENT_SUBMISSION == submissionType));
 			
 			// if the assignment is of attachment-only or allow both text and attachment, include option for uploading student attachment
-			context.put("includeSubmissionAttachment", Boolean.valueOf(Assignment.ATTACHMENT_ONLY_ASSIGNMENT_SUBMISSION == a.getContent().getTypeOfSubmission() || Assignment.TEXT_AND_ATTACHMENT_ASSIGNMENT_SUBMISSION == a.getContent().getTypeOfSubmission()));
+			context.put("includeSubmissionAttachment", Boolean.valueOf(Assignment.ATTACHMENT_ONLY_ASSIGNMENT_SUBMISSION == submissionType || Assignment.TEXT_AND_ATTACHMENT_ASSIGNMENT_SUBMISSION == submissionType || Assignment.SINGLE_ATTACHMENT_SUBMISSION == submissionType));
 		}
 		catch (IdUnusedException e)
 		{
