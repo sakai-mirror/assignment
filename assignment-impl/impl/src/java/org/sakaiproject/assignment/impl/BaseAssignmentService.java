35f6ae7caf026f0b679bc25b0c14d289940b6743
enietzel@anisakai.com <enietzel@anisakai.com@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2014-03-20 20:02:11 +0000

    SAK-25312 merge r307145 to 10.x
    git-svn-id: https://source.sakaiproject.org/svn/assignment/branches/sakai-10.x@307315 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- assignment-impl/impl/src/java/org/sakaiproject/assignment/impl/BaseAssignmentService.java
+++ assignment-impl/impl/src/java/org/sakaiproject/assignment/impl/BaseAssignmentService.java
@@ -5102,7 +5102,7 @@ public abstract class BaseAssignmentService implements AssignmentService, Entity
 			// now are we view all sections/groups or just specific one?
 			if (allOrOneGroup.equals(AssignmentConstants.ALL))
 			{
-				if (a.getAccess() ==  Assignment.AssignmentAccess.SITE)
+				if (allowAllGroups(contextString))
 				{
 					// site range
 					try {
